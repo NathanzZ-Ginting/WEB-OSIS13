b7728b7b4cb7a96a3c2fea962781467358797066
NathanzZ-Ginting <jojobrelingga@gmail.com>
2025-11-20 08:52:00 +0700

    edit

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { Navbar } from '../components/Navbar';
 import { ContactForm } from '../components/ContactForm';
 import { Footer } from '../components/Footer';
